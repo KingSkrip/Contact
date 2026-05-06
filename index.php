7e023c8af388f4c65d5240230b129cd5dfe78f1b
skrip5025@gmail.com <tuemail@dominio.com>
2026-05-06 10:09:20 -0600

    favicon.ico

--- index.php
+++ index.php
@@ -4,7 +4,9 @@
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>FIBRASAN</title>
+    <title>Fibrasan</title>
+
+    <link rel="icon" type="image/x-icon" href="/google/public/favicon.ico">
 
     <link rel="stylesheet" href="./css/layout.css" />
     <link rel="stylesheet" href="./css/historia/historia.css" />
